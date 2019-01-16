fcf9dd51ee2d841d0152ac55ba834f623e4e2202
GiantCowFilms <git@giantcowfilms.com>
2019-01-15 22:02:56 -0800

    Initialize Project

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const dist = path.resolve(__dirname, "dist");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+module.exports = {
+  entry: "./ts/index.ts",
+  output: {
+    path: dist,
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json", ".wasm"]
+  },
+  devServer: {
+    contentBase: dist,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, "crate")
+    }),
+  ]
+};
