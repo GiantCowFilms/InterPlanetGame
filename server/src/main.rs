0099d73d9aa7e87586234ccceea24da62e9327f6
GiantCowFilms <git@giantcowfilms.com>
2019-10-09 23:37:48 -0700

    Remove map server
    It seems eaiser to just do this with the websocket rather than messing
    around with an HTTP server inside the application.

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,16 +5,13 @@ extern crate serde_derive;
 extern crate tokio;
 
 mod game_server;
-mod map_server;
 mod game;
 
 use self::game_server::map_manager;
 use self::game_server::GameServer;
-use self::map_server::MapServer;
 
 fn main() {
     println!("Hello, world!");
-    bootstrap_map_server();
     bootstrap_game_servers()
 }
 
@@ -26,13 +23,4 @@ fn bootstrap_game_servers() {
             "Q:\\Projects\\Development\\2019\\inter-planet-game\\maps".to_string(),
         ),
     );
-}
-
-fn bootstrap_map_server() {
-    MapServer::start(
-        5665,
-        map_manager::FileSystemMapManager::new(
-            "Q:\\Projects\\Development\\2019\\inter-planet-game\\maps".to_string(),
-        ),
-    )
 }
