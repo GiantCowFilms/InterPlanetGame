3212bbef5e3d653a658bf04e6d0f6292fd8c2497
GiantCowFilms <git@giantcowfilms.com>
2019-11-01 23:08:30 -0700

    Refactor message handler to use try operator
    Every mutex lock, option unwrap, etc. were resulting in an additonal
    level of indentation in the message handler, which was resulting in
    rightward code drift. Now all the idents are gone, replaced with try
    macros, and the code is much cleaner.

--- server/src/game_server/connection.rs
+++ server/src/game_server/connection.rs
@@ -61,137 +61,111 @@ where
         message: &'a Message,
     ) -> impl Future<Output = ()> + Captures<'a> + Captures<'b> + 'c {
         async move {
-            let result = if let Ok(message_body) = message.to_text() {
-                if let Ok(message_data) = serde_json::from_str::<MessageType>(message_body) {
-                    match message_data {
-                        MessageType::CreateGame(game_settings) => {
-                            let mut sink = self.sink.lock().unwrap();
-                            match self.instance.map_manager.lock() {
-                                Ok(maps) => { 
-                                    if let Some(map) = maps.map_by_id(&game_settings.map_id) {
-                                        let game = Game::new((*map).clone(),game_settings.config);
-                                        match self.instance.games.write() {
-                                            Ok(mut games) => {
-                                                let game_id = games.add_game(game);
-                                                let seralized = serde_json::to_string(&MessageType::NewGame(GameMetadata {
-                                                    game_id
-                                                }));
-                                                let _ = sink.start_send(Message::from(seralized.unwrap()));
-                                                Ok(())
-                                            }
-                                            _=> Err("Game state corrupted by poisoned mutex. Please report this bug.".to_string())
-                                        }
-                                    } else {
-                                        Err(format!("Map with id \"{}\" not found.", game_settings.map_id))
-                                    }
-                                }
-                                Err(_poisoned) => Err("The game state is corrupted by a poisoned mutex. Please report this bug.".to_string())
-                            }
-                        }
-                        MessageType::ExitGame => {
-                            let mut sink = self.sink.lock().unwrap();
-                            let _ = sink.start_send(Message::from("ExitGame"));
-                            Ok(())
-                        }
-                        MessageType::EnterGame(game_metadata) => {
-                            let mut sink = self.sink.lock().unwrap();
-                            if let Ok(mut games) = self.instance.games.write() {
-                                if let Some(game_executor_mtx) =
-                                    games.get_mut(&game_metadata.game_id)
-                                {
-                                    let mut game_executor = game_executor_mtx.lock().unwrap();
-                                    if let Some(player) = &self.player {
-                                        self.player = Some(
-                                            game_executor
-                                                .add_player(player.clone())
-                                                .expect("Too many players"),
-                                        );
-                                        let handler_sink = self.sink.clone();
-                                        game_executor.event_source.on_event(Box::new(
-                                            move |event: &GameEvent, game: &mut Game| {
-                                                GameConnection::handle_game_event(
-                                                    handler_sink.clone(),
-                                                    game,
-                                                    event,
-                                                );
-                                            },
-                                        ));
-                                        let seralized = serde_json::to_string(
-                                            &MessageType::EnterGame(GameMetadata {
-                                                game_id: game_metadata.game_id.clone(),
-                                            }),
-                                        );
-                                        let _ = sink.start_send(Message::from(seralized.unwrap()));
-                                        if let Some(player) = &self.player {
-                                            let seralized = serde_json::to_string(
-                                                &MessageType::Possession(player.index as u32),
-                                            );
-                                            let _ =
-                                                sink.start_send(Message::from(seralized.unwrap()));
-                                        };
-                                        if game_executor.game.state.is_some() {
-                                            let seralized = serde_json::to_string(
-                                                &MessageType::Game(game_executor.game.clone()),
-                                            );
-                                            let _ =
-                                                sink.start_send(Message::from(seralized.unwrap()));
-                                        };
-                                        self.current_game = Some(game_executor_mtx.clone());
-                                        // Subscribe to game state
-                                        Ok(())
-                                    } else {
-                                        Err("Players must set a name before joining a game."
-                                            .to_string())
-                                    }
-                                } else {
-                                    Err(format!(
-                                        "Could not find a game with an id of \"{}\"",
-                                        &game_metadata.game_id
-                                    ))
-                                }
-                            } else {
-                                Err("RwLock poisoned, game state corrupted".to_string())
-                            }
-                            //Send game state
-                        }
-                        MessageType::SetName(name_data) => {
-                            //Replace player to avoid mutexes/refcells and such
-                            self.player = Some(Player {
-                                name: name_data.name,
-                                index: 0, //Garbag data
-                            });
-                            Ok(())
-                        }
-                        MessageType::StartGame => self
-                            .current_game
-                            .as_ref()
-                            .ok_or_else(|| "Player is not currently in a game".to_string())
-                            .and_then(|game_executor| {
-                                game_executor
-                                    .lock()
-                                    .map_err(|_| "Poisoned mutex".to_string())?
-                                    .start_game()
-                            }),
-                        MessageType::GameMove(game_move) => self
-                            .current_game
-                            .as_ref()
-                            .ok_or_else(|| "Player is not currently in a game".to_string())
-                            .and_then(|game_executor_mtx| {
-                                let mut game_executor = game_executor_mtx
-                                    .lock()
-                                    .map_err(|_| "Poisoned mutex".to_string())?;
-                                let timed_move =
-                                    game_executor.create_move(game_move.from, game_move.to)?;
-                                game_executor.add_move(self.player.as_ref().unwrap(), timed_move)
-                            }),
-                        _ => Err("The provided message type was not found.".to_string()),
+            let result = || -> Result<(),String> { 
+                let message_body = message.to_text().map_err(|_| "The recieved message could not be parsed as a string.".to_owned())?;
+                let message_data = serde_json::from_str::<MessageType>(message_body).map_err(|_| "Could not parse the provided message.".to_owned())?;
+                //Inside message handlers, always lock sinks first to avoid deadlocks
+                match message_data {
+                    MessageType::CreateGame(game_settings) => {
+                        let mut sink = self.sink.lock().unwrap();
+                        let maps = self.instance.map_manager.lock().map_err(|_| "Mutex Poisoned".to_owned())?;
+                        let map = maps.map_by_id(&game_settings.map_id).ok_or_else(|| format!("Map with id \"{}\" not found.", game_settings.map_id))?;
+                        let game = Game::new((*map).clone(),game_settings.config);
+                        let mut games = self.instance.games.write().map_err(|_| "Game state corrupted by poisoned mutex. Please report this bug.".to_owned())?;
+                        let game_id = games.add_game(game);
+                        let seralized = serde_json::to_string(&MessageType::NewGame(GameMetadata {
+                            game_id
+                        }));
+                        let _ = sink.start_send(Message::from(seralized.unwrap()));
+                        Ok(())
                     }
-                } else {
-                    Err("Could not parse the provided message.".to_string())
+                    MessageType::ExitGame => {
+                        let mut sink = self.sink.lock().unwrap();
+                        let _ = sink.start_send(Message::from("ExitGame"));
+                        Ok(())
+                    }
+                    MessageType::EnterGame(game_metadata) => {
+                        let mut sink = self.sink.lock().unwrap();
+                        let mut games = self.instance.games.write().map_err(|_| "Game state corrupted by poisoned mutex. Please report this bug.".to_owned())?;
+                        let game_executor_mtx = games.get_mut(&game_metadata.game_id).ok_or_else(|| format!(
+                            "Could not find a game with an id of \"{}\"",
+                            &game_metadata.game_id
+                        ))?;
+                        let mut game_executor = game_executor_mtx.lock().unwrap();
+                        let player = self.player.as_ref().ok_or_else(|| "Players must set a name before joining a game.".to_owned())?;
+                        self.player = Some(
+                            game_executor
+                                .add_player(player.clone())
+                                .expect("Too many players"),
+                        );
+                        let handler_sink = self.sink.clone();
+                        game_executor.event_source.on_event(Box::new(
+                            move |event: &GameEvent, game: &mut Game| {
+                                GameConnection::handle_game_event(
+                                    handler_sink.clone(),
+                                    game,
+                                    event,
+                                );
+                            },
+                        ));
+                        let seralized = serde_json::to_string(
+                            &MessageType::EnterGame(GameMetadata {
+                                game_id: game_metadata.game_id.clone(),
+                            }),
+                        );
+                        let _ = sink.start_send(Message::from(seralized.unwrap()));
+                        if let Some(player) = &self.player {
+                            let seralized = serde_json::to_string(
+                                &MessageType::Possession(player.index as u32),
+                            );
+                            let _ =
+                                sink.start_send(Message::from(seralized.unwrap()));
+                        };
+                        if game_executor.game.state.is_some() {
+                            let seralized = serde_json::to_string(
+                                &MessageType::Game(game_executor.game.clone()),
+                            );
+                            let _ =
+                                sink.start_send(Message::from(seralized.unwrap()));
+                        };
+                        self.current_game = Some(game_executor_mtx.clone());
+                        // Subscribe to game state
+                        Ok(())
+                        //Send game state
+                    }
+                    MessageType::SetName(name_data) => {
+                        //Replace player to avoid mutexes/refcells and such
+                        self.player = Some(Player {
+                            name: name_data.name,
+                            index: 0, //Garbag data
+                        });
+                        Ok(())
+                    }
+                    MessageType::StartGame => self
+                        .current_game
+                        .as_ref()
+                        .ok_or_else(|| "Player is not currently in a game".to_owned())
+                        .and_then(|game_executor| {
+                            game_executor
+                                .lock()
+                                .map_err(|_| "Poisoned mutex".to_owned())?
+                                .start_game()
+                        }),
+                    MessageType::GameMove(game_move) => self
+                        .current_game
+                        .as_ref()
+                        .ok_or_else(|| "Player is not currently in a game".to_owned())
+                        .and_then(|game_executor_mtx| {
+                            let mut game_executor = game_executor_mtx
+                                .lock()
+                                .map_err(|_| "Poisoned mutex".to_owned())?;
+                            let timed_move =
+                                game_executor.create_move(game_move.from, game_move.to)?;
+                            game_executor.add_move(self.player.as_ref().unwrap(), timed_move)
+                        }),
+                    _ => Err("The provided message type was not found.".to_owned()),
                 }
-            } else {
-                Err("The recieved message could not be parsed as a string.".to_string())
-            };
+            }();
             match result {
                 Ok(_) => (),
                 Err(e) => {
@@ -221,7 +195,7 @@ where
                 let _ = sink.start_send(Message::from(seralized.unwrap()));
                 Ok(())
             }
-            Err(_) => Err("Game state corrupted by poisned mutex.".to_string()),
+            Err(_) => Err("Game state corrupted by poisned mutex.".to_owned()),
         };
         if let Err(err_msg) = result {
             let _ = sink.start_send(Message::from(err_msg));
