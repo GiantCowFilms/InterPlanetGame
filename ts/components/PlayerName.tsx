f18e5b933454e5992d3942f801919b1cc8c41aa0
GiantCowFilms <git@giantcowfilms.com>
2019-11-14 23:22:46 -0800

    Style PlayerName screen
    Center box, and stop it from spanning the entire width, which is
    unnecessary and ugly.

--- ts/components/PlayerName.tsx
+++ ts/components/PlayerName.tsx
@@ -12,11 +12,13 @@ function PlayerName (props: Props) {
         props.onSubmit(playerName);
     };
     return (
-        <div className="card card-inside">
-            <h3>Choose an Alias</h3>
-            <input onChange={(e) => setPlayerName(e.target.value)} value={playerName} />
-            <div onClick={setName} className="button">
-                Save
+        <div className="center-content">
+            <div className="card card-inside player-name">
+                <h3>Choose an Alias</h3>
+                <input onChange={(e) => setPlayerName(e.target.value)} value={playerName} />
+                <div onClick={setName} className="button">
+                    Save
+                </div>
             </div>
         </div>
     );
