d9eec38d67839077c55c8a124cdcb4bc36ae54e5
GiantCowFilms <git@giantcowfilms.com>
2020-08-21 23:46:25 -0700

    Refactor accessing games list into hook
    There is a bit of boilerplate code involved in getting the game list
    out of the game client. An event handler has to be added to pickup list
    change events so it can set the react state, and that handler needs to
    be cleaned up when the component unmounts.
    
    I refactored this boilerplate out into a seperate hook, which reduces
    the amount of noise in the components. It also makes the logic
    reuseable.

--- ts/components/GameList.tsx
+++ ts/components/GameList.tsx
@@ -2,20 +2,14 @@ import React, { useEffect, useState, useContext } from "react";
 import {gameConnectionSingleton} from "../connection/index";
 import GameForm from "./GameForm";
 import GameItem from "./GameItem";
+import { useGameList } from "../connection/hooks";
 
 interface Props {
 
 }
 
 function GameList() {
-    const [games, setGames] = useState([]);
-    useEffect(() => {
-        const updateGames = () => {
-            setGames(gameConnectionSingleton.client.game_list());
-        };
-        gameConnectionSingleton.onEvent("GameList", updateGames);
-        gameConnectionSingleton.onEvent("NewGame", updateGames);
-    }, []);
+    const [games, setGames] = useGameList();
     return <div className="inner-content start-page">
         <GameForm />
         <div className="game-list">
---
+++ ts/connection/hooks.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { gameConnectionSingleton } from ".";
+
+export const useGameList = () => {
+    const state = useState([]);
+    const [games,setGames] = state;
+    useEffect(() => {
+        const updateGames = () => {
+            setGames(gameConnectionSingleton.client.game_list());
+        };
+        const remove1 = gameConnectionSingleton.onEvent("GameList", updateGames);
+        const remove2 = gameConnectionSingleton.onEvent("NewGame", updateGames);
+        return () => {
+            remove1();
+            remove2();
+        };
+    }, [setGames]);
+    return state;
+}
